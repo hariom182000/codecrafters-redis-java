7ea789fc41d486a7b90a2ee8eec1d762521ae3f8
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-01 19:36:54 +0530

    milti-ping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,29 +5,18 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-
-        //  Uncomment this block to pass the first stage
-
-
-
         int port = 6379;
-
-
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
-            while (true) {
-                // Since the tester restarts your program quite often, setting SO_REUSEADDR
-                // ensures that we don't run into 'Address already in use' errors
-                // Wait for connection from client.
-                Socket clientSocket  = serverSocket.accept();
+            while (serverSocket.isBound() && !serverSocket.isClosed()) {
+                final Socket clientSocket = serverSocket.accept();
                 try {
                     if (clientSocket != null) {
+                        System.out.println("Starting socket == " + clientSocket.hashCode());
                         clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
                         clientSocket.close();
                     }
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     System.out.println("IOException: " + e.getMessage());
                 }
             }
