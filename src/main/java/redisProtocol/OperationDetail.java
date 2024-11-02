7ce28bd797d9ecbe1fd2c48fd41ace66b7abea61
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-02 13:54:08 +0530

    milti-ping

--- src/main/java/redisProtocol/OperationDetail.java
+++ src/main/java/redisProtocol/OperationDetail.java
@@ -3,9 +3,29 @@ package redisProtocol;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
+
 public class OperationDetail {
     private Operation operation;
     private String value;
+
+    public OperationDetail(final Operation operation, final String value) {
+        this.operation = operation;
+        this.value = value;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(final Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(final String value) {
+        this.value = value;
+    }
 }
