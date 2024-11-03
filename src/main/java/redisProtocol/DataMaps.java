b7f455fee7700037be6ecb4d04008b839ca17621
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-03 13:23:11 +0530

    expiry

--- src/main/java/redisProtocol/DataMaps.java
+++ src/main/java/redisProtocol/DataMaps.java
@@ -1,14 +1,25 @@
 package redisProtocol;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DataMaps {
-    final Map<String, String> stringMap = new HashMap<>();
-    final Map<String, String> keyDataTypeMap = new HashMap<>();
+    final Map<String, String> stringMap = new ConcurrentHashMap<>();
+    final Map<String, String> keyDataTypeMap = new ConcurrentHashMap<>();
+    final Map<String, Long> keyTimeStamp = new ConcurrentHashMap<>();
+    final Map<String, Long> keyTtl = new ConcurrentHashMap<>();
+
+    public Map<String, Long> getKeyTimeStamp() {
+        return keyTimeStamp;
+    }
+
+    public Map<String, Long> getKeyTtl() {
+        return keyTtl;
+    }
 
     public Map<String, String> getStringMap() {
         return stringMap;
+
     }
 
     public Map<String, String> getKeyDataTypeMap() {
--- src/main/java/redisProtocol/RequestParser.java
+++ src/main/java/redisProtocol/RequestParser.java
@@ -3,15 +3,17 @@ package redisProtocol;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import java.util.Stack;
 
 public class RequestParser {
     private BufferedWriter writer;
     private BufferedReader reader;
     private DataMaps dataMaps;
-    private final Stack<String> commands = new Stack<>();
-    private final Stack<OperationDetail> operationDetails = new Stack<>();
+    private final List<Object> commands = new ArrayList<>();
+    private final List<OperationDetail> operationDetails = new ArrayList<>();
+    private int commnadSize = 0;
 
     public RequestParser(final BufferedWriter writer, final BufferedReader reader, final DataMaps dataMaps) {
         this.reader = reader;
@@ -30,49 +32,88 @@ public class RequestParser {
             System.out.println("timestamp ::: " + System.currentTimeMillis());
             System.out.println("message ::" + content);
             if (content.charAt(0) == '*') {
-                operationDetails.push(new OperationDetail(Operation.ARRAY, content.substring(1)));
+                commnadSize = parseInteger(content);
+                //operationDetails.add(new OperationDetail(Operation.ARRAY, content.substring(1)));
             } else if (content.charAt(0) == '$') {
-                operationDetails.push(new OperationDetail(Operation.BULK_STRING, content.substring(1)));
+                operationDetails.add(new OperationDetail(Operation.BULK_STRING, content.substring(1)));
             } else if (content.charAt(0) == '+') {
-                understandCommand(parseSimpleString(content));
+                operationDetails.add(new OperationDetail(Operation.SIMPLE_STRING, null));
+                commands.add(parseSimpleString(content));
             } else if (content.charAt(0) == ':') {
-                understandCommand(parseInteger(content));
+                operationDetails.add(new OperationDetail(Operation.INTEGER, null));
+                commands.add(parseInteger(content));
             } else if (content.charAt(0) == '_') {
-                operationDetails.push(new OperationDetail(Operation.NULL, null));
+                operationDetails.add(new OperationDetail(Operation.NULL, null));
             } else if (content.charAt(0) == '#') {
-                operationDetails.push(new OperationDetail(Operation.BOOLEAN, null));
+                operationDetails.add(new OperationDetail(Operation.BOOLEAN, null));
             } else if (content.charAt(0) == ',') {
-                operationDetails.push(new OperationDetail(Operation.DOUBLE, null));
+                operationDetails.add(new OperationDetail(Operation.DOUBLE, null));
             } else {
                 parseData(content);
             }
+            if (commands.size() == commnadSize) processLastCommand();
         }
     }
 
-    private void understandCommand(final Object value) throws IOException {
-        final String command = commands.pop();
-        System.out.println("reading in commands " + command);
-        if ("ECHO".equalsIgnoreCase(command) && Objects.nonNull(value)) {
-            writer.write("$" + value.toString().length() + "\r\n" + value.toString() + "\r\n");
-            writer.flush();
-        } else if ("SET".equalsIgnoreCase(command)) {
-            reader.readLine();
-            final String data = reader.readLine();
-            dataMaps.getStringMap().put(value.toString(), data);
-            writer.write("+OK\r\n");
-            writer.flush();
-        } else if ("GET".equalsIgnoreCase(command)) {
-            final String key = value.toString();
-            if (dataMaps.getStringMap().containsKey(key)) {
-                final String data = dataMaps.getStringMap().get(key);
-                writer.write("$" + data.length() + "\r\n" + data + "\r\n");
-                writer.flush();
+    private void processLastCommand() throws IOException {
+        if (commands.isEmpty()) return;
+        if ("PING".equalsIgnoreCase((String) commands.get(0))) {
+            handlePingCommand();
+        } else if ("GET".equalsIgnoreCase((String) commands.get(0))) {
+            handleGetCommand();
+        } else if ("SET".equalsIgnoreCase((String) commands.get(0))) {
+            handleSetCommand();
+        } else if ("ECHO".equalsIgnoreCase((String) commands.get(0))) {
+            handleEchoCommand();
+        }
+        operationDetails.clear();
+        commnadSize = 0;
+        commands.clear();
+
+    }
+
+    private void handleGetCommand() throws IOException {
+        final String key = (String) commands.get(1);
+        if (dataMaps.getStringMap().containsKey(key)) {
+            final String data = dataMaps.getStringMap().get(key);
+            if (dataMaps.getKeyTtl().containsKey(key)) {
+                if (System.currentTimeMillis() - dataMaps.getKeyTimeStamp().get(key) >= dataMaps.getKeyTtl().get(key)) {
+                    writer.write("$-1\r\n");
+                } else {
+                    writer.write("$" + data.length() + "\r\n" + data + "\r\n");
+                }
             } else {
-                writer.write("$-1\r\n");
-                writer.flush();
+                writer.write("$" + data.length() + "\r\n" + data + "\r\n");
             }
+        } else {
+            writer.write("$-1\r\n");
 
         }
+        writer.flush();
+    }
+
+
+    private void handleSetCommand() throws IOException {
+        final String key = (String) commands.get(1);
+        final String value = (String) commands.get(2);
+        Long ttl = (long) -1;
+        if (commands.size() > 3) {
+            String px = (String) commands.get(3);
+            if ("px".equalsIgnoreCase(px)) {
+                ttl = Long.parseLong((String) commands.get(4));
+            }
+        }
+        dataMaps.getStringMap().put(key, value);
+        dataMaps.getKeyTimeStamp().put(key, System.currentTimeMillis());
+        if (ttl > 0) dataMaps.getKeyTtl().put(key, ttl);
+        writer.write("+OK\r\n");
+        writer.flush();
+    }
+
+    private void handleEchoCommand() throws IOException {
+        final String value = (String) commands.get(1);
+        writer.write("$" + value.length() + "\r\n" + value + "\r\n");
+        writer.flush();
     }
 
     private void handlePingCommand() throws IOException {
@@ -90,14 +131,10 @@ public class RequestParser {
 
 
     private void parseData(final String content) throws IOException {
-        final OperationDetail operationDetail = operationDetails.pop();
+        final OperationDetail operationDetail = operationDetails.getLast();
         if (Operation.BULK_STRING.equals(operationDetail.getOperation())) {
             if (operationDetail.getValue().equals(String.valueOf(content.length()))) {
-                System.out.println("pushing in commands" + content);
-                if (commands.isEmpty()) {
-                    if ("PING".equalsIgnoreCase(content)) handlePingCommand();
-                    else commands.push(content);
-                } else understandCommand(content);
+                commands.add(content);
             } else throw new RuntimeException();
         }
     }
