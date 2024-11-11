c0776bb97a79cdb8b7ee72820e67afb65e933b41
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-11 16:47:04 +0530

    diff ports

--- src/main/java/redisProtocol/RequestParser.java
+++ src/main/java/redisProtocol/RequestParser.java
@@ -93,11 +93,11 @@ public class RequestParser {
         Long length = 11L;
         if (dataMaps.getConfigMap().containsKey("master_repl_offset")) {
             data += "\r\nmaster_repl_offset:" + dataMaps.getConfigMap().get("master_repl_offset");
-            length += data.length();
+            length += "master_repl_offset:".length() + dataMaps.getConfigMap().get("master_repl_offset").length();
         }
         if (dataMaps.getConfigMap().containsKey("master_replid")) {
             data += "\r\nmaster_replid:" + dataMaps.getConfigMap().get("master_replid");
-            length += data.length();
+            length += "master_replid:".length() + dataMaps.getConfigMap().get("master_replid").length();
         }
         System.out.println("data with length is " + length + "  " + data);
         return "$" + length + "\r\n" + data + "\r\n";
