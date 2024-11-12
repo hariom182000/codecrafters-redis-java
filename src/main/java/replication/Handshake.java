4f70517850f0dd3fdddf853005a4245c2813a668
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-12 18:55:27 +0530

    [any message]

--- src/main/java/replication/Handshake.java
+++ src/main/java/replication/Handshake.java
@@ -17,12 +17,16 @@ public class Handshake {
     }
 
     public void connect() throws IOException {
-        final String[] addressDetail = dataMaps.getConfigMap().get("replicaof").split("\\s+");
+        final String[] addressDetail = dataMaps.getConfigMap().get("replicaof").split("\s+");
         System.out.println("address details " + addressDetail[0] + " " + addressDetail[1]);
         Socket clientSocket = new Socket(addressDetail[0], Integer.parseInt(addressDetail[1]));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         out.write("*1\r\n$4\r\nPING\r\n");
+        out.flush();
+        out.write("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n"+dataMaps.getConfigMap().get("port")+"\r\n");
+        out.flush();
+        out.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
         out.flush();
     }
 }
