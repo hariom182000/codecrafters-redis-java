78a348da64a4ac05a9eb565f5694fab1d3f0ff9e
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-30 17:24:23 +0530

    wait for replica

--- src/main/java/redisProtocol/Parser.java
+++ src/main/java/redisProtocol/Parser.java
@@ -37,10 +37,16 @@ public class Parser {
             System.out.println("message ::" + content);
             ParserUtils.readCommand(content, operationDetails, commands);
             if (commands.size() == commnadSize) {
-                System.out.println("final message read ::" + request);
-                operationDetails.clear();
-                commands.add(request);
-                return commands;
+                try {
+                    System.out.println("final message read ::" + request);
+                    operationDetails.clear();
+                    commands.add(request);
+                    return commands;
+
+                } catch (final Exception e) {
+                    System.out.println("could not read " + e);
+                }
+
             }
         }
         return commands;
