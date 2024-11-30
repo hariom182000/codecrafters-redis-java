a9d68ef9bbbbe9bef5738218ff7849f973aac3be
hariom.sharma <hariom.sharma@flipkart.com>
2024-11-30 17:21:02 +0530

    wait for replica

--- src/main/java/redisProtocol/ParserUtils.java
+++ src/main/java/redisProtocol/ParserUtils.java
@@ -118,7 +118,7 @@ public class ParserUtils {
             replicaSocket.getOutputStream().flush();
             final Parser parser = new Parser(new BufferedReader(new InputStreamReader(replicaSocket.getInputStream())));
             List<Object> response = parser.help();
-            System.out.println("response from replica is ...");
+            System.out.println("response from replica is ..."+response.getLast());
             if ("REPLCONF".equalsIgnoreCase((String) response.get(0)) && "ACK".equalsIgnoreCase((String) response.get(1))) {
                 System.out.println("process by replicas ");
                 if (bytesSendToReplicas.get() <= (long) response.get(2)) replicasAcked.addAndGet(1);
